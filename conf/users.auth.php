e219dac495e41652b49666e8a47264e18823ca3a
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2018-05-08 14:11:38 -0400

    first commit, pre-update, using git-lfs

---
+++ conf/users.auth.php
@@ -0,0 +1,11 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+mezka:21232f297a57a5a743894a0e4a801fc3:Emiliano Mesquita:emiliano_mesquita@hotmail.com:admins
+calidadmesquita:21232f297a57a5a743894a0e4a801fc3:Facundo Vaccaro:calidad@mesquita.com.ar:admins
